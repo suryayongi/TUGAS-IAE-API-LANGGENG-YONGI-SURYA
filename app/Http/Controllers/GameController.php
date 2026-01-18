2fef18c6800e28aff582b750904b2692ada72d6f
Langgeng Yongi Surya <langstar20@gmail.com>
2026-01-19 04:58:23 +0700

    PENAMBAHAN PERANCANGAN API UNTUK TUGAS 2

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,30 +3,67 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http; 
+use Illuminate\Support\Facades\Http;
 
 class GameController extends Controller
 {
     public function index()
     {
-        // 1. Ambil API Key dari .env
+        $apiKey = env('RAWG_API_KEY');
+
+        $response = Http::get("https://api.rawg.io/api/games", [
+            'key' => $apiKey,
+            'page_size' => 12,
+            'ordering' => '-added'
+        ]);
+
+        if ($response->successful()) {
+            $games = $response->json()['results'];
+        } else {
+            $games = [];
+        }
+
+        return view('games.index', ['games' => $games]);
+    }
+    public function indexApi()
+    {
+        // 1. Ambil API Key
         $apiKey = env('RAWG_API_KEY');
 
         // 2. Tembak API RAWG
         $response = Http::get("https://api.rawg.io/api/games", [
             'key' => $apiKey,
-            'page_size' => 12,
-            'ordering' => '-added' 
+            'page_size' => 10,
+            'ordering' => '-added'
         ]);
 
-        // 3. Cek jika API berhasil dihubungi
+        // 3. Cek respons
         if ($response->successful()) {
             $games = $response->json()['results'];
-        } else {
-            $games = []; 
-        }
+            $formattedGames = collect($games)->map(function ($game) {
+                return [
+                    'judul_game' => $game['name'],
+                    'rating' => $game['rating'],
+                    'tanggal_rilis' => $game['released'],
+                    'gambar' => $game['background_image'],
+                    'sumber_data' => 'RAWG API via Langgeng Server'
+                ];
+            });
 
-        // 4. Kirim data ke tampilan 
-        return view('games.index', ['games' => $games]);
+            // 5. Kembalikan dalam bentuk JSON
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Data game berhasil diambil',
+                'total_data' => count($formattedGames),
+                'data' => $formattedGames
+            ], 200);
+
+        } else {
+            // kalo misal gagal
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Gagal mengambil data dari RAWG'
+            ], 500);
+        }
     }
 }
---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GameController;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/games', [GameController::class, 'indexApi']);
